8bb1dfcdce5cd7f738c33a5c81c6b0642db22e75
Antonio Santos <aj.santos128@gmail.com>
2026-09-03 13:49:58 -0400

    remove blogroll from models

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -15,7 +15,6 @@ class StaticViewSitemap(Sitemap):
             "blog:now",
             "blog:archive",
             "blog:notes",
-            "blog:blogroll",
             "blog:subscribe",
         ]
 
